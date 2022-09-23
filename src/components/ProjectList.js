0ccf861d0953bd3b7731c9e8ecccef7c6cead1e2
david-commit <david.ondiege@students.moringaschool.com>
2022-09-23 22:02:17 +0300

    Completed assignment

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -2,13 +2,25 @@ import React from "react";
 import ProjectItem from "./ProjectItem";
 
 function ProjectList({ projects }) {
-  console.log(projects);
-  return (
-    <div id="projects">
-      <h2>My Projects</h2>
-      <div id="project-list">{/* render ProjectItem components here */}</div>
-    </div>
-  );
+
+    console.log(projects);
+    return (
+      <div id='projects'>
+        <h2>My Projects</h2>
+        <div id='project-list'>
+          {projects.map((project) => (
+            <ProjectItem
+              name={project.name}
+              key={project.id}
+              project={project.name}
+              about={project.about}
+              technologies={project.technologies}
+            />
+          ))}
+        </div>
+      </div>
+    );
+
 }
 
 export default ProjectList;
